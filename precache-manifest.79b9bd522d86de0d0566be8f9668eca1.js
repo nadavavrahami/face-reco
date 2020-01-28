1043535d129409a9b8b1afeb14b2ec9ca567b3c0
nadavavrahami <nadav170293@gmail.com>
2020-01-28 21:24:42 +0200

    Updates

---
+++ precache-manifest.79b9bd522d86de0d0566be8f9668eca1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab28d39ba575d76acc866d45faabbac",
+    "url": "/face-reco/index.html"
+  },
+  {
+    "revision": "927703584be9c760c67f",
+    "url": "/face-reco/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fd672098002c4be43d85",
+    "url": "/face-reco/static/css/main.fa1c716c.chunk.css"
+  },
+  {
+    "revision": "927703584be9c760c67f",
+    "url": "/face-reco/static/js/2.69ad6fd3.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/face-reco/static/js/2.69ad6fd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd672098002c4be43d85",
+    "url": "/face-reco/static/js/main.d5801fff.chunk.js"
+  },
+  {
+    "revision": "eaa5d6559d518efac94f",
+    "url": "/face-reco/static/js/runtime-main.706943f9.js"
+  },
+  {
+    "revision": "7c2262a7307c69d92197ba46db1596c1",
+    "url": "/face-reco/static/media/logoIcon.7c2262a7.jpeg"
+  }
+]);
